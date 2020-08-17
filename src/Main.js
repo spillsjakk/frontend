e05e35a84a26c479c161545f11a8ee957d8afaf6
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 00:59:58 +0200

    Add contact page

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 const Main = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home}></Route>
+      <Route exact path='/contact' component={Contact}></Route>
     </Switch>
   );
 }
---
+++ src/pages/Contact.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from "react";
+import { Helmet } from 'react-helmet';
+
+class Contact extends PureComponent {
+  render() {
+    return (
+      <>
+        <Helmet>
+          <title>Home page</title>
+        </Helmet>
+        <p class="mt-5">
+          <strong>Kontakt SkoleSjakken</strong>
+        </p>
+        <p>
+          Jorunn Brekke<br />
+          Prosjektleder<br />
+          Epost: <a href="mailto:jorunn%40sjakk.no">jorunn@sjakk.no</a><br />
+          Telefon: 948 222 86
+        </p>
+      </>
+    );
+  }
+}
+
+export default Contact;
