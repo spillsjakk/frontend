d73ba8a5f5a679b5ea728f865d78c3f1289a1a77
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 00:51:08 +0200

    Use Helmet for titles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React, { PureComponent } from "react";
+import { Helmet } from 'react-helmet';
 
 class Home extends PureComponent {
   render() {
     return (
       <>
+        <Helmet>
+          <title>Home page</title>
+        </Helmet>
         <p>
           This is the homepage.
         </p>
