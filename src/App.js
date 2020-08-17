e32fc94b204e332da87c174b5feef7e3236b4342
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 01:23:25 +0200

    Put Main in Container, no div around everything

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Main from './Main';
 
+import { Container } from "react-bootstrap";
+
 import Menu from './components/Menu';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Menu />
-      <Main />
-    </div>
+      <Container>
+        <Main />
+      </Container>
+    </>
   );
 }
 
