f2be7cd8633838764d857a5c96a128a6a8f6f819
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 01:22:23 +0200

    First version of navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Main from './Main';
 
+import Menu from './components/Menu';
+
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Main />
     </div>
   );
---
+++ src/components/Menu.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import { Navbar, Nav } from "react-bootstrap";
+
+class Menu extends Component {
+  render() {
+    return (
+      <Navbar expand="lg">
+        <Navbar.Brand as={Link} to="/">Spill<b>Sjakk</b></Navbar.Brand>
+        <Navbar.Toggle aria-controls="collapsible-navbar" />
+        <Navbar.Collapse id="collapsible-navbar">
+          <Nav className="mr-auto"></Nav>
+          <Nav className="mr-1">
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Menu;
