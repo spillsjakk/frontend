ef87f0c17a7c331f9d40c32023069d1dfbcf014f
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 00:41:11 +0200

    Implement routing to homepage

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+const Main = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={Home}></Route>
+    </Switch>
+  );
+}
+
+export default Main;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <>
+        <p>
+          This is the homepage.
+        </p>
+      </>
+    );
+  }
+}
+
+export default Home;
