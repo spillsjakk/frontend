62e6b6ff3ff4dec1ba5c8414a20c1e2f80a84fd1
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 17:30:15 +0200

    Profile page search field

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,9 +8,13 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 
+import ToolkitProvider, { Search, SearchMatchProps } from 'react-bootstrap-table2-toolkit';
+
+const { SearchBar } = Search;
+
 type ProfileState = {
   name: string,
-  tournamentData: any[],
+  tournamentData: Tournament[],
   tournamentColumns: any[],
   gameData: any[],
   gameColumns: any[]
@@ -43,12 +47,20 @@ class Profile extends Component<RouteComponentProps<ProfileParams>, ProfileState
     super(props);
     this.state = {
       name: "", tournamentData: [], tournamentColumns: [
-        { dataField: "tournament", text: Translated.byKey("tournament") },
-        { dataField: "startDate", text: Translated.byKey("startDate"), sort: true },
-        { dataField: "endDate", text: Translated.byKey("endDate"), sort: true }
+        {
+          dataField: "tournament", text: Translated.byKey("tournament"), formatter: function (_: any, row: Tournament, __: any, ___: any) {
+            return <Link to={"/tournament/view/" + row.id}>{row.name}</Link>
+          }
+        },
+        { dataField: "start_date", text: Translated.byKey("startDate"), sort: true },
+        { dataField: "end_date", text: Translated.byKey("endDate"), sort: true }
       ],
       gameData: [], gameColumns: [
-        { dataField: "game", text: Translated.byKey("game") },
+        {
+          dataField: "game", text: Translated.byKey("game"), formatter: function (_: any, row: Tournament, __: any, ___: any) {
+            return <Link to={"/game/view/" + row.id}>{row.name}</Link>
+          }
+        },
         { dataField: "start", text: Translated.byKey("dateTime"), sort: true },
         { dataField: "tournament", text: Translated.byKey("tournament"), sort: true },
         { dataField: "timeControl", text: Translated.byKey("timeControl"), sort: true },
@@ -57,27 +69,27 @@ class Profile extends Component<RouteComponentProps<ProfileParams>, ProfileState
     };
   }
 
+  onColumnMatch({
+    searchText,
+    value,
+    column,
+    row
+  }: SearchMatchProps<any>) {
+    return (value && value.toLowerCase().includes(searchText)) ||
+      (row.id.toLowerCase().includes(searchText) || row.name.toLowerCase().includes(searchText));
+  }
+
   componentDidMount() {
     document.getElementsByTagName("body")[0].id = "Profile";
 
     fetchJson("/s/profile/" + this.props.match.params.uid, "GET", undefined, data => {
-      const tournaments: Tournament[] = data.tournaments;
-      const tournamentData = tournaments.map(t => {
-        let elem = <Link to={"/tournament/view/" + t.id}>{t.name}</Link>;
-        return {
-          id: t.id,
-          tournament: elem,
-          startDate: t.start_date,
-          endDate: t.end_date
-        };
-      });
+      const tournamentData = data.tournaments;
 
       const games: Game[] = data.games;
       const gameData = games.map(g => {
-        let elem = <Link to={"/game/view" + g.id}>{g.white_name} - {g.black_name}</Link>;
         return {
           id: g.id,
-          game: elem,
+          name: g.white_name + " - " + g.black_name,
           start: g.start.replace("T", " ").replace("Z", "").split(".")[0],
           tournament: g.tournament_name,
           timeControl: g.initial_time.toString() + "+" + g.increment.toString(),
@@ -110,19 +122,40 @@ class Profile extends Component<RouteComponentProps<ProfileParams>, ProfileState
         <h1 className="mt-4 p-3">
           {this.state.name}
         </h1>
-        <BootstrapTable
+
+        <div className="mt-5"></div>
+
+        <ToolkitProvider
           keyField="id"
           data={this.state.tournamentData}
           columns={this.state.tournamentColumns}
           bootstrap4={true}
-          pagination={ paginationFactory({}) } />
+          search={{ onColumnMatch: this.onColumnMatch }}
+        >
+          {props =>
+            <>
+              <SearchBar {...props.searchProps} />
+              <BootstrapTable {...props.baseProps} pagination={paginationFactory({})} />
+            </>
+          }
+        </ToolkitProvider>
+
         <div className="mt-5"></div>
-        <BootstrapTable
+
+        <ToolkitProvider
           keyField="id"
           data={this.state.gameData}
           columns={this.state.gameColumns}
           bootstrap4={true}
-          pagination={ paginationFactory({}) } />
+          search={{ onColumnMatch: this.onColumnMatch }}
+        >
+          {props =>
+            <>
+              <SearchBar {...props.searchProps} />
+              <BootstrapTable {...props.baseProps} pagination={paginationFactory({})} />
+            </>
+          }
+        </ToolkitProvider>
       </>
     );
   }
