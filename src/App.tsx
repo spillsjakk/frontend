a0fa6257569b8fdabfc4d33ca37fc174f5489d8d
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 11:19:34 +0200

    Change App from function to Component class

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Main from './Main';
 
 import { Container } from "react-bootstrap";
@@ -7,26 +7,36 @@ import Menu from './components/Menu';
 import LogoFooter from './components/LogoFooter';
 import LangContext from './components/LangContext';
 
-function App() {
-  const [lang, setLang] = useState(localStorage.getItem("lang") ?? "EN");
-  const setLangWithLocalStorage = (newLang: string) => {
-    setLang(newLang);
-    localStorage.setItem("lang", newLang);
-  };
-  const langProviderValue = { lang, setLang: setLangWithLocalStorage };
-  return (
-    <>
-      <LangContext.Provider value={langProviderValue}>
-        <Container id="main-container">
-          <Menu />
-          <Container id="content-container">
-            <Main />
+type AppState = {
+  lang: string
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { lang: localStorage.getItem("lang") ?? "EN" };
+  }
+
+  render() {
+    const setLangWithLocalStorage = (newLang: string) => {
+      this.setState({ lang: newLang });
+      localStorage.setItem("lang", newLang);
+    };
+    const langProviderValue = { lang: this.state.lang, setLang: setLangWithLocalStorage };
+    return (
+      <>
+        <LangContext.Provider value={langProviderValue}>
+          <Container id="main-container">
+            <Menu />
+            <Container id="content-container">
+              <Main />
+            </Container>
           </Container>
-        </Container>
-      </LangContext.Provider>
-      <LogoFooter />
-    </>
-  );
+        </LangContext.Provider>
+        <LogoFooter />
+      </>
+    );
+  }
 }
 
 export default App;
