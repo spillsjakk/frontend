83d255a226ec0a3d57abbb53b78657bfeefa5369
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 10:14:35 +0200

    Add carousel to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,72 @@
 import React, { PureComponent } from "react";
 import { Helmet } from 'react-helmet';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./Home.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
 
-class Home extends PureComponent {
+import Translated from '../components/Translated';
+
+class Home extends Component {
   render() {
     return (
       <>
         <Helmet>
           <title>Home page</title>
         </Helmet>
-        <p>
-          This is the homepage.
-        </p>
+
+        <div className="mt-5 carousel-container">
+          <Carousel autoPlay={true} interval={10000}>
+            <div>
+              <div className="carousel-image-container">
+                <img src="/icons/carousel/management-svgrepo-com.svg" />
+              </div>
+              <h3 className="mt-3"><Translated str="carousel1" /></h3>
+              <p>
+                <Translated str="carousel1Desc" />
+              </p>
+              <p>
+                <Link to="/about"><em><Translated str="about" />...</em></Link>
+              </p>
+            </div>
+            <div>
+              <div className="carousel-image-container">
+                <img src="/icons/carousel/hammer-and-wrench-doodle-svgrepo-com.svg" />
+              </div>
+              <h3 className="mt-3"><Translated str="carousel2" /></h3>
+              <p>
+                <Translated str="carousel2Desc" />
+              </p>
+              <p>
+                <Link to="/about"><em><Translated str="about" />...</em></Link>
+              </p>
+            </div>
+            <div>
+              <div className="carousel-image-container">
+                <img src="/icons/carousel/chess-board-svgrepo-com.svg" />
+              </div>
+              <h3 className="mt-3"><Translated str="carousel3" /></h3>
+              <p>
+                <Translated str="carousel3Desc" />
+              </p>
+              <p>
+                <Link to="/about"><em><Translated str="about" />...</em></Link>
+              </p>
+            </div>
+            <div>
+              <div className="carousel-image-container">
+                <img src="/icons/carousel/podium-svgrepo-com.svg" />
+              </div>
+              <h3 className="mt-3"><Translated str="carousel4" /></h3>
+              <p>
+                <Translated str="carousel4Desc" />
+              </p>
+              <p>
+                <Link to="/about"><em><Translated str="about" />...</em></Link>
+              </p>
+            </div>
+          </Carousel>
+        </div>
       </>
     );
   }
