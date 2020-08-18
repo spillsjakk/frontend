38f6755ddf1d27f22635d92a42cb9abe348a764d
Thomas Daniels <daniels.thomas@pm.me>
2020-08-18 02:12:45 +0200

    noopener noreferrer on _blank links

--- src/components/LogoFooter.js
+++ src/components/LogoFooter.js
@@ -7,9 +7,9 @@ class LogoFooter extends Component {
       <footer>
         <Container>
           <div className="d-flex flex-row justify-content-around">
-            <div><a href="https://www.sjakk.no/nsf/" target="_blank"><img src="/icons/logos/NSF.png" className="nsf" /></a></div>
-            <div><a href="https://www.skolesjakken.no/" target="_blank"><img src="/icons/logos/SkoleSjakken.png" /></a></div>
-            <div><a href="https://www.sjakk.no/usf/" target="_blank"><img src="/icons/logos/USF.png" className="usf" /></a></div>
+            <div><a href="https://www.sjakk.no/nsf/" target="_blank" rel="noopener noreferrer"><img src="/icons/logos/NSF.png" className="nsf" /></a></div>
+            <div><a href="https://www.skolesjakken.no/" target="_blank" rel="noopener noreferrer"><img src="/icons/logos/SkoleSjakken.png" /></a></div>
+            <div><a href="https://www.sjakk.no/usf/" target="_blank" rel="noopener noreferrer"><img src="/icons/logos/USF.png" className="usf" /></a></div>
           </div>
         </Container>
       </footer>
