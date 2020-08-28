fb50677d73fc835835fad76adc5d033032b32ccf
Thomas Daniels <daniels.thomas@pm.me>
2020-08-29 00:24:28 +0200

    Allow changing name/description from team management page

--- src/pages/Team/Manage.tsx
+++ src/pages/Team/Manage.tsx
@@ -1,4 +1,4 @@
-import React, { Component, RefObject } from "react";
+import React, { Component, RefObject, ChangeEvent, FormEvent } from "react";
 import { Helmet } from 'react-helmet';
 import Translated from "../../components/Translated";
 import { Team, Account } from "../Tournament/Types";
@@ -17,7 +17,9 @@ type ManageState = {
     team: Team,
     members: TeamMember[]
     not_members: string[][]
-  }
+  },
+  newName: string,
+  newDescription: string
 };
 
 class Manage extends Component<RouteComponentProps<ManageProps>, ManageState> {
@@ -28,12 +30,14 @@ class Manage extends Component<RouteComponentProps<ManageProps>, ManageState> {
     super(props);
 
     this.teamId = this.props.match.params.tid;
-    this.state = { loaded: false };
+    this.state = { loaded: false, newName: "", newDescription: "" };
     this.customAccRef = React.createRef();
 
     this.addMember = this.addMember.bind(this);
     this.removeMember = this.removeMember.bind(this);
     this.loadState = this.loadState.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.submitInfo = this.submitInfo.bind(this);
   }
 
   addMember(uid: string) {
@@ -49,7 +53,26 @@ class Manage extends Component<RouteComponentProps<ManageProps>, ManageState> {
   }
 
   loadState() {
-    fetchJson(`/s/team/manage/${this.props.match.params.tid}`, "GET", undefined, result => this.setState({ loaded: true, info: result }));
+    fetchJson(`/s/team/manage/${this.props.match.params.tid}`, "GET", undefined, result => {
+      this.setState({ loaded: true, info: result, newName: result.team.name, newDescription: result.team.description });
+    });
+  }
+
+  handleChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const newState: any = {};
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+
+  submitInfo(e: FormEvent) {
+    e.preventDefault();
+
+    fetchJson(`/s/team/edit/${this.teamId}`, "POST", { name: this.state.newName, description: this.state.newDescription }, _ => {
+      const info = { ...this.state.info! };
+      info.team.name = this.state.newName;
+      info.team.description = this.state.newDescription;
+      this.setState({ info });
+    });
   }
 
   componentDidMount() {
@@ -69,6 +92,20 @@ class Manage extends Component<RouteComponentProps<ManageProps>, ManageState> {
           <title>{info.team.name}</title>
         </Helmet>
         <h1 className="mt-4 p-3"><Translated str="manage" /> &quot;{info.team.name}&quot;</h1>
+
+        <form className="mt-4" onSubmit={this.submitInfo}>
+          <div className="form-group">
+            <label htmlFor="newName"><Translated str="name" />:</label>&nbsp;
+            <input type="text" className="form-control w-50" name="newName" id="newName" required value={this.state.newName} onChange={this.handleChange} />
+          </div>
+          <div className="form-group mt-4">
+            <label htmlFor="newDescription"><Translated str="description" />:</label>
+            <textarea className="form-control w-50" name="newDescription" id="newDescription" required value={this.state.newDescription} onChange={this.handleChange} />
+          </div>
+          <div className="mt-4">
+            <button className="btn btn-primary" type="submit"><Translated str="update" /></button>
+          </div>
+        </form>
 
         <h3 className="mt-4"><Translated str="members" /></h3>
 
