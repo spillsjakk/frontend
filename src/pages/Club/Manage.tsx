dd70c53fc09048588110a69a0c3a31bbb8276ce3
Thomas Daniels <daniels.thomas@pm.me>
2020-08-28 18:55:26 +0200

    Start on club manage page

---
+++ src/pages/Club/Manage.tsx
@@ -0,0 +1,100 @@
+import React, { PureComponent, ChangeEvent, FormEvent } from "react";
+import { Helmet } from 'react-helmet';
+import { Container } from 'react-bootstrap';
+import Translated from "../../components/Translated";
+import { fetchJson } from "../../functions";
+
+type ManageState = {
+  exists: boolean
+  id: string
+  name: string
+  description: string
+  country: string
+  region: string
+}
+
+class Manage extends PureComponent<{}, ManageState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      exists: false,
+      id: "",
+      name: "",
+      description: "",
+      country: "",
+      region: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  componentDidMount() {
+    document.getElementsByTagName("body")[0].id = "NotFound";
+
+    fetchJson("/s/club/manage", "GET", undefined, result => {
+      if (result.length > 0) {
+        this.setState({
+          id: result[0].id,
+          name: result[0].name,
+          description: result[0].description,
+          country: result[0].country,
+          region: result[0].region
+        });
+      }
+    });
+  }
+
+  handleChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const newState: any = {};
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+
+  submit(e: FormEvent) {
+    e.preventDefault();
+  
+    fetchJson(`/s/club/edit`, "POST", this.state, _ => {
+      this.setState({ exists: true });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Helmet>
+          <title>Manage clubs</title>
+        </Helmet>
+        <h1 className="mt-5 p-3"><Translated str="manageClub" /></h1>
+        <form className="mt-5" onSubmit={this.submit}>
+          <div className="form-group">
+            <label htmlFor="description"><Translated str="name" />:</label>
+            <input type="text" className="form-control" id="name" name="name" required value={this.state.name} onChange={this.handleChange} />
+          </div>
+          <div className="form-group mt-4">
+            <label htmlFor="id"><Translated str="id" />:</label>
+            <input type="text" id="id" className="form-control w-25" name="id" required value={this.state.id} disabled={this.state.exists} onChange={this.handleChange} />
+          </div>
+          <div className="form-group mt-4">
+            <label htmlFor="description"><Translated str="description" />:</label>
+            <textarea id="description" className="form-control w-25" name="description" required value={this.state.description} onChange={this.handleChange} />
+          </div>
+          <div className="form-group mt-4">
+            <label htmlFor="country"><Translated str="country" />:</label>
+            <input type="text" id="country" className="form-control w-25" name="country" required value={this.state.country} onChange={this.handleChange} />
+          </div>
+          <div className="form-group mt-4">
+            <label htmlFor="region"><Translated str="region" />:</label>
+            <input type="text" id="region" className="form-control w-25" name="region" required value={this.state.region} onChange={this.handleChange} />
+          </div>
+          <div className='mt-4'>
+            <button type="submit" className="btn btn-primary"><Translated str="submit" /></button>
+          </div>
+        </form>
+      </>
+    );
+  }
+}
+
+export default Manage;
