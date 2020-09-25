124058498e676c7d1122f96ceab399ff6113e21a
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-09-25 18:52:03 +0300

    update: footer logos appear only on /about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React, { useState, Component } from 'react';
-import Main from './Main';
+import React, { Component } from "react";
+import Main from "./Main";
 
 import { Container } from "react-bootstrap";
 
-import Menu from './components/Menu';
-import LogoFooter from './components/LogoFooter';
-import LangContext from './components/LangContext';
-import { UserContext, UserContextDataType } from './components/UserContext';
+import Menu from "./components/Menu";
+import LangContext from "./components/LangContext";
+import { UserContext, UserContextDataType } from "./components/UserContext";
 
-import { fetchJson } from './functions';
-import GameNotifier from './components/GameNotifier';
+import { fetchJson } from "./functions";
+import GameNotifier from "./components/GameNotifier";
 
 type AppState = {
-  lang: string,
-  user: UserContextDataType
-}
+  lang: string;
+  user: UserContextDataType;
+};
 
 class App extends Component<{}, AppState> {
   constructor(props: {}) {
@@ -22,13 +21,15 @@ class App extends Component<{}, AppState> {
     this.state = {
       lang: localStorage.getItem("lang") ?? "EN",
       user: {
-        authenticated: false
-      }
+        authenticated: false,
+      },
     };
   }
 
   componentDidMount() {
-    fetchJson("/s/account/auth-info", "GET", undefined, data => this.setState({ user: data }));
+    fetchJson("/s/account/auth-info", "GET", undefined, (data) =>
+      this.setState({ user: data })
+    );
   }
 
   render() {
@@ -36,8 +37,14 @@ class App extends Component<{}, AppState> {
       this.setState({ lang: newLang });
       localStorage.setItem("lang", newLang);
     };
-    const langProviderValue = { lang: this.state.lang, setLang: setLangWithLocalStorage };
-    const userProviderValue = { user: this.state.user, setUser: (u: UserContextDataType) => this.setState({ user: u })};
+    const langProviderValue = {
+      lang: this.state.lang,
+      setLang: setLangWithLocalStorage,
+    };
+    const userProviderValue = {
+      user: this.state.user,
+      setUser: (u: UserContextDataType) => this.setState({ user: u }),
+    };
     return (
       <>
         <LangContext.Provider value={langProviderValue}>
@@ -51,7 +58,6 @@ class App extends Component<{}, AppState> {
             </Container>
           </UserContext.Provider>
         </LangContext.Provider>
-        <LogoFooter />
       </>
     );
   }
--- src/pages/Tournament/build/index.tsx
+++ src/pages/Tournament/build/index.tsx
@@ -1,8 +1,11 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import { BuildTournament as Container } from "../../../containers/build-tournament/index";
 import { WithBuildTournamentForm } from "../../../hocs/build-tournament-form/index";
 
 const BuildTournament: FunctionComponent<{}> = () => {
+  useEffect(() => {
+    document.getElementsByTagName("body")[0].id = "Tournament-Build";
+  }, []);
   return (
     <>
       <WithBuildTournamentForm>
