f9138e58d9f4fb32573d8902d011eb60ee7519dd
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-11-07 18:06:51 +0300

    add: box css class is added

--- src/containers/home/features.tsx
+++ src/containers/home/features.tsx
@@ -37,7 +37,7 @@ const Features: FunctionComponent<{}> = () => {
     <Carousel
       activeIndex={index}
       onSelect={onCarouselSelect}
-      className={`${style["features-carousel"]} box-shadow`}
+      className={`${style["features-carousel"]} box`}
     >
       {Array.isArray(data) &&
         data.map((datum, i) => (
