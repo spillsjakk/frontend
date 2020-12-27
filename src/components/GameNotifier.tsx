8ec133ae352b8b88fb696fea118a0b7381cbd715
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-12-27 13:49:08 +0300

    update(game notifier): game notifier box is made sticky

--- src/components/GameNotifier.tsx
+++ src/components/GameNotifier.tsx
@@ -1,18 +1,26 @@
 import React, { Component, PureComponent } from "react";
 import { Container, Alert } from "react-bootstrap";
 import Translated from "./translated";
-import { Link, useHistory, withRouter, RouteComponentProps } from "react-router-dom";
+import {
+  Link,
+  useHistory,
+  withRouter,
+  RouteComponentProps,
+} from "react-router-dom";
 import { fetchJson } from "../functions";
 import { UserContext } from "./UserContext";
 
 type GameNotifierState = {
-  shouldPlay: boolean
-  mayShowNotification: boolean
-  gameId: string
-  unlisten?: Function
-}
+  shouldPlay: boolean;
+  mayShowNotification: boolean;
+  gameId: string;
+  unlisten?: Function;
+};
 
-class GameNotifier extends PureComponent<RouteComponentProps, GameNotifierState> {
+class GameNotifier extends PureComponent<
+  RouteComponentProps,
+  GameNotifierState
+> {
   static contextType = UserContext;
   context!: React.ContextType<typeof UserContext>;
 
@@ -24,30 +32,36 @@ class GameNotifier extends PureComponent<RouteComponentProps, GameNotifierState>
     this.state = {
       shouldPlay: false,
       mayShowNotification: true,
-      gameId: ""
+      gameId: "",
     };
 
     this.checkForGame = this.checkForGame.bind(this);
   }
 
   componentDidMount() {
-    this.setState({
-      unlisten: this.props.history.listen((location, _) => {
-        if (!this.context.user.authenticated) {
-          return;
-        }
+    this.setState(
+      {
+        unlisten: this.props.history.listen((location, _) => {
+          if (!this.context.user.authenticated) {
+            return;
+          }
 
-        if (location.pathname === "/game/lobby" || location.pathname === "/game/play/" + this.state.gameId) {
-          this.setState({ mayShowNotification: false });
-        } else {
-          this.setState({ mayShowNotification: true });
-          this.checkForGame();
-        }
-      }),
-    }, () => {
-      this.checkForGame(true);
-      window.setInterval(this.checkForGame, 30000);
-    });
+          if (
+            location.pathname === "/game/lobby" ||
+            location.pathname === "/game/play/" + this.state.gameId
+          ) {
+            this.setState({ mayShowNotification: false });
+          } else {
+            this.setState({ mayShowNotification: true });
+            this.checkForGame();
+          }
+        }),
+      },
+      () => {
+        this.checkForGame(true);
+        window.setInterval(this.checkForGame, 30000);
+      }
+    );
   }
 
   checkForGame(firstTime?: boolean) {
@@ -55,15 +69,19 @@ class GameNotifier extends PureComponent<RouteComponentProps, GameNotifierState>
       return;
     }
 
-    fetchJson(`/s/game/lobby`, "GET", undefined, result => {
+    fetchJson(`/s/game/lobby`, "GET", undefined, (result) => {
       if (result.authenticated === false) {
         return;
       }
 
       if (result.next !== -1 && result.next <= 600000) {
         this.setState({
-          shouldPlay: true, gameId: result.id,
-          mayShowNotification: !(this.props.location.pathname === "/game/lobby" || this.props.location.pathname === "/game/play/" + result.id)
+          shouldPlay: true,
+          gameId: result.id,
+          mayShowNotification: !(
+            this.props.location.pathname === "/game/lobby" ||
+            this.props.location.pathname === "/game/play/" + result.id
+          ),
         });
       } else {
         this.setState({ shouldPlay: false, gameId: "" });
@@ -82,11 +100,23 @@ class GameNotifier extends PureComponent<RouteComponentProps, GameNotifierState>
     }
 
     return (
-      <Alert variant="warning" className="mt-3" style={{ textAlign: "center" }}>
+      <Alert
+        variant="warning"
+        className="mt-3"
+        style={{
+          textAlign: "center",
+          position: "sticky",
+          top: "75px",
+          zIndex: 20,
+        }}
+      >
         <strong>
-          <Translated str="aboutToPlay" /> <Link to="/game/lobby"><Translated str="goToLobby" /></Link>
+          <Translated str="aboutToPlay" />{" "}
+          <Link to="/game/lobby">
+            <Translated str="goToLobby" />
+          </Link>
         </strong>
-      </Alert >
+      </Alert>
     );
   }
 }
