f82239a20fffc0a8377b9ada283cb5b2b8389be7
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2021-03-14 08:30:49 +0300

    fix - pagination for miniboards is added

--- src/components/miniboards/index.tsx
+++ src/components/miniboards/index.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Pagination } from "@material-ui/lab";
 import { Board } from "./board";
 import style from "./style.module.scss";
 
@@ -8,17 +9,36 @@ interface Props {
 }
 
 const Miniboards: FunctionComponent<Props> = ({ data }) => {
+  const [page, setPage] = useState(1);
+  const [boardsToShow, setBoardsToShow] = useState([]);
+
+  useEffect(() => {
+    if (Array.isArray(data)) {
+      setBoardsToShow(
+        data.filter((game, index) => {
+          if ((page - 1) * 12 <= index && index < page * 12) {
+            return game;
+          }
+        })
+      );
+    }
+  }, [page]);
   return (
     <>
       <Container id={style.miniboards}>
         <Row>
-          {data &&
-            data.map((game) => (
+          {Array.isArray(boardsToShow) &&
+            boardsToShow.map((game) => (
               <Col key={game.id} md="3">
                 <Board game={game} />
               </Col>
             ))}
         </Row>
+        <Pagination
+          count={Array.isArray(data) ? Math.ceil(data.length / 12) : 0}
+          page={page}
+          onChange={(e, value) => setPage(value)}
+        />
       </Container>
     </>
   );
