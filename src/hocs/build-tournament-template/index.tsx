1d25095a7522e5f981edce1fa0a94f84d1672198
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2021-05-08 17:00:03 +0300

    tournament templates are changed

--- src/hocs/build-tournament-template/index.tsx
+++ src/hocs/build-tournament-template/index.tsx
@@ -1,20 +1,22 @@
 import React, { FunctionComponent, useCallback, useState } from "react";
+import Translated from "../../components/translated";
 import { useUser } from "../../components/UserContext";
+import { KIND } from "../../constants";
 import { useForm } from "../../context/build-tournament-form";
 import { TemplateProvider } from "../../context/build-tournament-template";
 import { generateId } from "../../functions";
 
 const templates = [
   {
-    name: "90 minutes + 30 seconds / Individual / 9 rounds / Swiss",
+    name: "9 rounds Swiss 3 minutes + 2 seconds",
     value: 1,
   },
   {
-    name: "15 minutes + 10 seconds / Individual / 7 rounds / Swiss",
+    name: "6 rounds rapid Swiss 15 minutes + 10 seconds",
     value: 2,
   },
   {
-    name: "3 minutes + 2 seconds / Individual / 11 rounds / Swiss ",
+    name: "4 rounds rapid SkoleSjakken 10 minutes + 0 seconds",
     value: 3,
   },
 ];
@@ -22,21 +24,27 @@ const templates = [
 const templateData = [
   {
     id: 1,
-    initialTime: 90,
-    increment: 30,
+    initialTime: 3,
+    increment: 2,
     numberOfRounds: 9,
+    kind: KIND.SwissDutch,
+    kindName: "swissDutch",
   },
   {
     id: 2,
     initialTime: 15,
     increment: 10,
-    numberOfRounds: 7,
+    numberOfRounds: 6,
+    kind: KIND.SwissDutch,
+    kindName: "swissDutch",
   },
   {
     id: 3,
-    initialTime: 3,
+    initialTime: 10,
     increment: 2,
-    numberOfRounds: 11,
+    numberOfRounds: 4,
+    kind: KIND.TeamMonrad,
+    kindName: "teamMonrad",
   },
 ];
 
@@ -57,18 +65,28 @@ const WithBuildTournamentTemplate: FunctionComponent = ({ children }) => {
         return;
       }
 
-      const name = `${user?.info?.name}: ${template.initialTime}+${template.increment} Individual ${template.numberOfRounds}-round Swiss`;
+      const name = `${user?.info?.name}: ${Translated.byKey(
+        template.kindName
+      )} ${template.initialTime}+${template.increment} ${
+        template.numberOfRounds
+      }-round `;
       form.changeName(name);
       form.changeDescription(name);
       form.changeId(generateId(7));
       form.changePubliclyViewable(true);
       form.changeSelfJoinable(true);
       form.changeFideRated(true);
-      form.changeKind(1);
+      form.changeKind(template.kind);
       form.changeRounds!(template.numberOfRounds);
       form.changeOnlinePairingIntervalN(5);
       form.changeInitialTime(template.initialTime);
       form.changeIncrement(template.increment);
+      if (template.kind === KIND.SwissDutch) {
+        form.changeTb1("0");
+        form.changeTb2("1");
+        form.changeTb3("2");
+        form.changeTb4("3");
+      }
 
       setSelectedTemplate(value);
     },
