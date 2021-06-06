7c44a2f95e00d7642ea5af1727bf28f338c5d754
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2021-06-06 19:08:10 +0300

    club statistics - mfRatio chart is implemented

--- src/pages/Club/manage/detail/stats.tsx
+++ src/pages/Club/manage/detail/stats.tsx
@@ -2,17 +2,39 @@ import React, { FunctionComponent, useEffect, useState } from "react";
 import { fetchCall } from "../../../../functions";
 import { useClub } from "../../../../context/club";
 import Translated from "../../../../components/translated";
-import { Bar, BarChart, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  Cell,
+  Pie,
+  PieChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const COLORS: string[] = ["#f7b7a3", "#ea5f89", "#9b3192", "#57167e"];
 
 const Stats: FunctionComponent<unknown> = () => {
   const [accounts, setAccounts] = useState([]);
+  const [mfRatio, setMfRatio] = useState([
+    { name: "M", value: 2 },
+    { name: "F", value: 4 },
+  ]);
   const [ageHistogram, setAgeHistogram] = useState([]);
   const { id, region } = useClub();
   function fetchAccounts() {
     fetchCall(`/s/club/all-accounts/${id}`, "GET", undefined, (response) => {
       setAccounts(response);
       const ageHistogram: { [age: string]: number } = {};
+      const mData = { name: "M", value: 0 };
+      const fData = { name: "F", value: 0 };
       for (const account of response) {
+        if (account.sex === "M") {
+          mData.value += 1;
+        } else if (account.sex === "F") {
+          fData.value += 1;
+        }
         if (account.birth_date) {
           const age = Math.floor(
             (new Date().getTime() - new Date(account.birth_date!).getTime()) /
@@ -29,6 +51,7 @@ const Stats: FunctionComponent<unknown> = () => {
           }
         }
       }
+      setMfRatio([mData, fData]);
       const sortedAges = Object.keys(ageHistogram)
         .map(Number)
         .sort((a, b) => a - b);
@@ -65,6 +88,24 @@ const Stats: FunctionComponent<unknown> = () => {
           <YAxis />
           <Bar dataKey="count" fill={"darkblue"} />
         </BarChart>
+        <strong>
+          <Translated str="mfRatio" />:
+        </strong>
+        <PieChart width={350} height={350}>
+          <Pie
+            data={mfRatio}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={150}
+          >
+            {mfRatio.map((o, index) => (
+              <Cell key={index} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
       </div>
     </>
   );
