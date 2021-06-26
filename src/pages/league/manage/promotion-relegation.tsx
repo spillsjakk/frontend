e1c546c4fce28fc61034af633dec25e134297fb9
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2021-06-27 02:02:49 +0300

    category step component is added, category participants are gotten

--- src/pages/league/manage/promotion-relegation.tsx
+++ src/pages/league/manage/promotion-relegation.tsx
@@ -5,14 +5,48 @@ import {
   Stepper,
   Typography,
 } from "@material-ui/core";
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import Translated from "../../../components/translated";
-import { useLeague } from "../../../hocs/with-league/index";
+import { Category, useLeague } from "../../../hocs/with-league/index";
 import style from "./style.module.scss";
+import { fetchJson } from "../../../functions";
+import { Participant } from "../../Tournament/Types";
 
 function Label({ text }: { text: string }) {
   return <div className={style.heading}>{text}</div>;
 }
+
+const CategoryStep: FunctionComponent<{ category: Category }> = (props) => {
+  const [participants, setParticipants] = useState<Array<Participant>>([]);
+
+  useEffect(() => {
+    fetchJson(
+      `/s/leagues/${props.category.league}/categories/${props.category.id}/participants`,
+      "GET",
+      undefined,
+      (response) => {
+        if (Array.isArray(response)) {
+          setParticipants(response);
+        }
+      }
+    );
+  }, []);
+  return (
+    <Step>
+      <StepLabel>
+        <Label text={props.category.name} />
+      </StepLabel>
+      <StepContent {...props}>
+        <div>
+          {Array.isArray(participants) &&
+            participants.map((participant) => (
+              <div key={participant.account}>{participant.account}</div>
+            ))}
+        </div>
+      </StepContent>
+    </Step>
+  );
+};
 
 const PromotionRelegationForm: FunctionComponent<unknown> = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -38,12 +72,7 @@ const PromotionRelegationForm: FunctionComponent<unknown> = () => {
         {league &&
           Array.isArray(league.categories) &&
           league.categories.map((category) => (
-            <Step key={category.id}>
-              <StepLabel>
-                <Label text={category.name} />
-              </StepLabel>
-              <StepContent></StepContent>
-            </Step>
+            <CategoryStep key={category.id} category={category} />
           ))}
       </Stepper>
       <Typography variant="h5">{Translated.byKey("relegation")}</Typography>
