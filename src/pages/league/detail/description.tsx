73b6c0fe5aeee9137f42aa29562fd8544f1a9b60
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2021-08-01 18:03:51 +0300

    build league and edit league pages - markdown editor is added for description field.

--- src/pages/league/detail/description.tsx
+++ src/pages/league/detail/description.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 import xssFilters from "xss-filters";
-
+import ReactMarkdown from "react-markdown";
 import style from "./style.module.scss";
 import { useLeague } from "../../../hocs/with-league/index";
 
@@ -10,11 +10,9 @@ const Description: FunctionComponent<{}> = () => {
   return (
     <div className={style["description-container"]}>
       <div className={style.about}>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: xssFilters.inHTMLData(league?.description || ""),
-          }}
-        ></div>
+        <ReactMarkdown linkTarget="_blank">
+          {xssFilters.inHTMLData(league?.description || "")}
+        </ReactMarkdown>
       </div>
     </div>
   );
