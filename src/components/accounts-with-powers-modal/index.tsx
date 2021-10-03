beae96f96e0b7d9e2783393364cc3959bdabbf09
hilalguvenc <hilalguvenc@yahoo.com>
2021-10-03 21:09:57 +0300

    Added clubpowers and orgpowers deletion functions

--- src/components/accounts-with-powers-modal/index.tsx
+++ src/components/accounts-with-powers-modal/index.tsx
@@ -9,6 +9,8 @@ import BootstrapTable from "react-bootstrap-table-next";
 import { Link } from "react-router-dom";
 import { useOrganization } from "../../context/organization";
 import { useClub } from "../../context/club";
+import { Delete } from "@material-ui/icons";
+import { IconButton } from "@material-ui/core";
 
 interface Props {
   forClub?: boolean;
@@ -27,55 +29,6 @@ function getUserLink(text: string, id: string) {
   );
 }
 
-const columns = [
-  {
-    dataField: "first_name",
-    text: Translated.byKey("firstName"),
-    sort: true,
-    formatter: function (_: any, row: Account, __: any, ___: any) {
-      return getUserLink(row.first_name, row.id);
-    },
-  },
-  {
-    dataField: "last_name",
-    text: Translated.byKey("lastName"),
-    sort: true,
-    formatter: function (_: any, row: Account, __: any, ___: any) {
-      return getUserLink(row.last_name, row.id);
-    },
-  },
-  {
-    dataField: "username",
-    text: Translated.byKey("username"),
-    sort: true,
-    formatter: function (_: any, row: Account, __: any, ___: any) {
-      return getUserLink(row.username, row.id);
-    },
-  },
-  {
-    dataField: "fide_number",
-    text: Translated.byKey("fideNumber"),
-    sort: true,
-  },
-  {
-    dataField: "fide_rating",
-    text: Translated.byKey("fideRating"),
-    sort: true,
-  },
-  { dataField: "title", text: Translated.byKey("title"), sort: true },
-  {
-    dataField: "fide_federation",
-    text: Translated.byKey("federation"),
-    sort: true,
-  },
-  {
-    dataField: "birth_date",
-    text: Translated.byKey("birthDate"),
-    sort: true,
-  },
-  { dataField: "sex", text: Translated.byKey("sex"), sort: true },
-];
-
 const AccountsWithPowersModal: FunctionComponent<Props> = ({ forClub }) => {
   const [allAccounts, setAllAccounts] = useState<Array<Account> | undefined>();
   const [arbiterAccounts, setArbiterAccounts] = useState<
@@ -88,7 +41,7 @@ const AccountsWithPowersModal: FunctionComponent<Props> = ({ forClub }) => {
   const organization = useOrganization();
   const club = useClub();
 
-  useEffect(() => {
+  function fetchAccountsWithPowers() {
     fetchCall(
       `/s/${forClub ? "club" : "organization"}/accounts-with-powers/${
         forClub ? club.id : organization.id
@@ -103,7 +56,112 @@ const AccountsWithPowersModal: FunctionComponent<Props> = ({ forClub }) => {
         }
       }
     );
+  }
+
+  useEffect(() => {
+    fetchAccountsWithPowers();
   }, []);
+
+  function deleteClubPower(clubId, packageName, userId) {
+    fetchCall(
+      `/s/account/power/club/${clubId} `,
+      "DELETE",
+      {
+        power: `club:${packageName}:${clubId}`,
+        id: userId,
+      },
+      () => {
+        fetchAccountsWithPowers();
+      }
+    );
+  }
+  
+  function deleteOrgPower(orgId, packageName, userId) {
+    fetchCall(
+      `/s/account/power/organization/${orgId} `,
+      "DELETE",
+      {
+        power: `organization:${packageName}:${orgId}`,
+        id: userId,
+      },
+      () => {
+        fetchAccountsWithPowers();
+      }
+    );
+  }
+  
+  const columns = [
+    {
+      dataField: "first_name",
+      text: Translated.byKey("firstName"),
+      sort: true,
+      formatter: function (_: any, row: Account, __: any, ___: any) {
+        return getUserLink(row.first_name, row.id);
+      },
+    },
+    {
+      dataField: "last_name",
+      text: Translated.byKey("lastName"),
+      sort: true,
+      formatter: function (_: any, row: Account, __: any, ___: any) {
+        return getUserLink(row.last_name, row.id);
+      },
+    },
+    {
+      dataField: "username",
+      text: Translated.byKey("username"),
+      sort: true,
+      formatter: function (_: any, row: Account, __: any, ___: any) {
+        return getUserLink(row.username, row.id);
+      },
+    },
+    {
+      dataField: "fide_number",
+      text: Translated.byKey("fideNumber"),
+      sort: true,
+    },
+    {
+      dataField: "fide_rating",
+      text: Translated.byKey("fideRating"),
+      sort: true,
+    },
+    { dataField: "title", text: Translated.byKey("title"), sort: true },
+    {
+      dataField: "fide_federation",
+      text: Translated.byKey("federation"),
+      sort: true,
+    },
+    {
+      dataField: "birth_date",
+      text: Translated.byKey("birthDate"),
+      sort: true,
+    },
+    { dataField: "sex", text: Translated.byKey("sex"), sort: true },
+    {
+      dataField: "",
+      text: Translated.byKey(""),
+      sort: false,
+      formatter: function (_: any, row: any) {
+        return (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <IconButton aria-label="delete">
+              <Delete
+                color="action"
+                onClick={() => {
+                  if (row.forClub) {
+                    deleteClubPower(row.entityId, row.packageName, row.id);
+                  } else {
+                    deleteOrgPower(row.entityId, row.packageName, row.id);
+                  }
+                }}
+              />
+            </IconButton>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
     <>
       {Array.isArray(allAccounts) && allAccounts.length > 0 && (
@@ -113,7 +171,12 @@ const AccountsWithPowersModal: FunctionComponent<Props> = ({ forClub }) => {
           </div>
           <ToolkitProvider
             keyField="username"
-            data={allAccounts}
+            data={allAccounts.map((d) => ({
+              ...d,
+              entityId: forClub ? club.id : organization.id,
+              packageName: "all",
+              forClub,
+            }))}
             columns={columns}
             bootstrap4={true}
             search={true}
@@ -137,7 +200,12 @@ const AccountsWithPowersModal: FunctionComponent<Props> = ({ forClub }) => {
           </div>
           <ToolkitProvider
             keyField="username"
-            data={arbiterAccounts}
+            data={arbiterAccounts.map((d) => ({
+              ...d,
+              entityId: forClub ? club.id : organization.id,
+              packageName: "arbiter",
+              forClub,
+            }))}
             columns={columns}
             bootstrap4={true}
             search={true}
@@ -161,7 +229,12 @@ const AccountsWithPowersModal: FunctionComponent<Props> = ({ forClub }) => {
           </div>
           <ToolkitProvider
             keyField="username"
-            data={editorAccounts}
+            data={editorAccounts.map((d) => ({
+              ...d,
+              entityId: forClub ? club.id : organization.id,
+              packageName: "editor",
+              forClub,
+            }))}
             columns={columns}
             bootstrap4={true}
             search={true}
