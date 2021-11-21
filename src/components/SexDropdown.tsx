1c017917718414ccb1feb1288350c5b4b4813500
hilalguvenc <hilal.guvenc@trendyol.com>
2021-11-21 19:58:19 +0300

    Create accounts tables are completed

--- src/components/SexDropdown.tsx
+++ src/components/SexDropdown.tsx
@@ -1,4 +1,6 @@
+import { NativeSelect } from "@material-ui/core";
 import React, { ChangeEvent } from "react";
+import Translated from "./translated";
 export default function SexDropdown(props: {
   className?: string,
   value?: string,
@@ -6,13 +8,39 @@ export default function SexDropdown(props: {
   id?: string,
   onChange?: (event: ChangeEvent<HTMLSelectElement>) => void
 }) {
-  return <select value={props.value}
-    onChange={props.onChange}
-    className={props.className}
-    name={props.name}
-    id={props.id}
-  >
-    <option value="M">M</option>
-    <option value="F">F</option>
-  </select>
+  return (
+    <select
+      value={props.value}
+      onChange={props.onChange}
+      className={props.className}
+      name={props.name}
+      id={props.id}
+    >
+      <option value="M">M</option>
+      <option value="F">F</option>
+    </select>
+  );
 }
+export function MuiSexDropdown(props: {
+  className?: string;
+  value?: string;
+  name?: string;
+  id?: string;
+  onChange?: (event: ChangeEvent<HTMLSelectElement>) => void;
+}) {
+  return (
+    <NativeSelect
+      variant="outlined"
+      value={props.value}
+      onChange={props.onChange}
+      className={props.className}
+      name={props.name}
+      id={props.id}
+    >
+      <option value="" disabled>{Translated.byKey("sex")}</option>
+      <option value="M">M</option>
+      <option value="F">F</option>
+    </NativeSelect>
+  );
+}
+
--- src/pages/Account/Create.tsx
+++ src/pages/Account/Create.tsx
@@ -7,23 +7,25 @@ import React, {
 import { Helmet } from "react-helmet";
 import Translated from "../../components/translated";
 import { useUser } from "../../components/UserContext";
-import UserLink from "../../components/UserLink";
+import { Link } from "react-router-dom";
 import { fetchJson, title as titleFn } from "../../functions";
 import "./Create.css";
-import FederationDropdown from "../../components/FederationDropdown";
-import TitleDropdown from "../../components/TitleDropdown";
-import { NativeSelect } from "@material-ui/core";
-import SexDropdown from "../../components/SexDropdown";
+import { MuiFederationDropdown } from "../../components/FederationDropdown";
+import { MuiTitleDropdown } from "../../components/TitleDropdown";
+import { Button, Grid, NativeSelect, TextField } from "@material-ui/core";
+import { MuiSexDropdown } from "../../components/SexDropdown";
 import { HelpBox, helpboxNames } from "../../components/help-box";
 import style from "./style.module.scss";
 import {
   useOrgsClubs,
   WithUserOrgsClubs,
 } from "../../hocs/user-orgs-and-clubs";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
-function Required() {
-  return <span style={{ color: "red" }}>(required)</span>;
-}
+const commonFields = {
+  headerClassName: "table-header",
+  cellClassName: "table-cell",
+};
 
 function SelectClubs(props: {
   value: string;
@@ -58,7 +60,6 @@ function SelectClubs(props: {
     </div>
   );
 }
-
 const Create: FunctionComponent<{}> = () => {
   const [username, setUserName] = useState("");
   const [first_name, setFirstName] = useState("");
@@ -78,6 +79,137 @@ const Create: FunctionComponent<{}> = () => {
   );
   const [selectedClub, setSelectedClub] = useState("");
   const { user } = useUser();
+
+  const accountsColumns: GridColDef[] = [
+    {
+      field: "username",
+      headerName: Translated.byKey("username"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      minWidth: 140,
+      flex: 1,
+      ...commonFields,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/profile/" + params.row.id}>
+              {params.row.username}
+            </Link>
+          </>
+        );
+      },
+    },
+    {
+      field: "first_name",
+      headerName: Translated.byKey("firstName"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 140,
+      ...commonFields,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/profile/" + params.row.id}>
+              {params.row.first_name}
+            </Link>
+          </>
+        );
+      },
+
+    },
+    {
+      field: "last_name",
+      headerName: Translated.byKey("lastName"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 140,
+      ...commonFields,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/profile/" + params.row.id}>
+              {params.row.last_name}
+            </Link>
+          </>
+        );
+      },
+    },
+    {
+      field: "fide_number",
+      headerName: Translated.byKey("fideNumber"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 180,
+      ...commonFields,
+    },
+    {
+      field: "title",
+      headerName: Translated.byKey("title"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 140,
+      ...commonFields,
+    },
+    {
+      field: "fide_rating",
+      headerName: Translated.byKey("fideRating"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 180,
+      ...commonFields,
+    },
+    {
+      field: "fide_federation",
+      headerName: Translated.byKey("fideFederation"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 180,
+      ...commonFields,
+    },
+    {
+      field: "birth_date",
+      headerName: Translated.byKey("birthDate"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 140,
+      ...commonFields,
+    },
+    {
+      field: "sex",
+      headerName: Translated.byKey("sex"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 140,
+      ...commonFields,
+    },
+    {
+      field: "email",
+      headerName: Translated.byKey("email"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 140,
+      ...commonFields,
+    },
+    {
+      field: "level",
+      headerName: Translated.byKey("level"),
+      hideSortIcons: true,
+      align: "center",
+      headerAlign: "center",
+      width: 140,
+      ...commonFields,
+    },
+  ];
 
   function fideNumberBlur() {
     if (isNaN(parseInt(fide_number as string, 10))) {
@@ -159,6 +291,23 @@ const Create: FunctionComponent<{}> = () => {
     document.getElementsByTagName("body")[0].id = "Account-Create";
   }, []);
 
+  const accountData = accounts.map((account, i) => {
+    return {
+      id: account.id,
+      username: account.username,
+      first_name: account.first_name,
+      last_name: account.last_name,
+      fide_number: account.fide_number,
+      title: account.title,
+      fide_rating: account.fide_rating,
+      fide_federation: account.fide_federation,
+      birth_date: account.birth_date,
+      sex: account.sex,
+      email: account.email,
+      level: account.level,
+    }
+  })
+
   return (
     <>
       <Helmet>
@@ -170,236 +319,176 @@ const Create: FunctionComponent<{}> = () => {
       </div>
 
       <form onSubmit={addNewAcc}>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">
-                <Translated str="username" />
-                <Required />
-              </th>
-              <th scope="col">
-                <Translated str="firstName" />
-                <Required />
-              </th>
-              <th scope="col">
-                <Translated str="lastName" />
-                <Required />
-              </th>
-              <th scope="col">
-                <Translated str="fideNumber" />
-              </th>
-              <th scope="col">
-                <Translated str="title" />
-              </th>
-              <th scope="col">
-                <Translated str="fideRating" />
-              </th>
-              <th scope="col">
-                <Translated str="fideFederation" />
-              </th>
-              <th scope="col">
-                <Translated str="birthDate" />
-                <Required />
-              </th>
-              <th scope="col">
-                <Translated str="sex" />
-              </th>
-              <th scope="col">
-                <Translated str="email" />
-              </th>
-              <th scope="col">
-                <Translated str="permissions" />
-              </th>
-              <th scope="col">
-                <Translated str="selectClub" />
-                <Required />
-              </th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {accounts.map((account, i) => (
-              <tr key={i}>
-                <td>
-                  <UserLink
-                    id={account.id}
-                    name={account.username}
-                    ghost={false}
-                  />
-                </td>
-                <td>
-                  <UserLink
-                    id={account.id}
-                    name={account.first_name}
-                    ghost={false}
-                  />
-                </td>
-                <td>
-                  <UserLink
-                    id={account.id}
-                    name={account.last_name}
-                    ghost={false}
-                  />
-                </td>
-                <td>{account.fide_number}</td>
-                <td>{account.title}</td>
-                <td>{account.fide_rating}</td>
-                <td>{account.fide_federation}</td>
-                <td>{account.birth_date}</td>
-                <td>{account.sex}</td>
-                <td>{account.email}</td>
-                <td>{account.level}</td>
-              </tr>
-            ))}
-            <tr>
+        <Grid container spacing={3} item justifyContent="center">
+          <Grid item xs={12} lg={4} md={6}>
+            <TextField
+              label={Translated.byKey("username")}
+              id="username"
+              name="username"
+              inputProps={{ pattern: "^[a-zA-Z0-9-_]+$" }}
+              fullWidth
+              variant="outlined"
+              type="text"
+              required
+              value={username}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} lg={4} md={6}>
+            <TextField
+              label={Translated.byKey("firstName")}
+              type="text"
+              id="firstNameInput"
+              name="first_name"
+              value={first_name}
+              onChange={(e) => setFirstName(e.target.value)}
+              fullWidth
+              variant="outlined"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} lg={4} md={6}>
+            <TextField
+              label={Translated.byKey("lastName")}
+              type="text"
+              id="lastNameInput"
+              name="last_name"
+              value={last_name}
+              onChange={(e) => setLastName(e.target.value)}
+              fullWidth
+              variant="outlined"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} lg={4} md={6}>
+            <TextField
+              label={Translated.byKey("email")}
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12} lg={4} md={6}>
+            <TextField
+              label={Translated.byKey("fideNumber")}
+              type="number"
+              id="fideNumberInput"
+              name="fide_number"
+              value={fide_number}
+              onChange={(e) => setFideNumber(e.target.value)}
+              onBlur={fideNumberBlur}
+              fullWidth
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12} lg={4} md={6}>
+            <HelpBox
+              placement="bottom"
+              name={helpboxNames.createAccountsInputs}
+              text={Translated.byKey("createAccountsInputsHelpbox")}
+              show={true}
+            >
               <td>
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  pattern="^[a-zA-Z0-9-_]+$"
-                  value={username}
-                  onChange={(e) => setUserName(e.target.value)}
-                  required
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  id="firstNameInput"
-                  name="first_name"
-                  value={first_name}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  required
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  id="lastNameInput"
-                  name="last_name"
-                  value={last_name}
-                  onChange={(e) => setLastName(e.target.value)}
-                  required
-                />
-              </td>
-              <td>
-                <input
+                <TextField
+                  label={Translated.byKey("fideRating")}
                   type="number"
-                  id="fideNumberInput"
-                  name="fide_number"
-                  value={fide_number}
-                  onChange={(e) => setFideNumber(e.target.value)}
-                  onBlur={fideNumberBlur}
+                  id="fideRatingInput"
+                  name="fide_rating"
+                  value={fide_rating}
+                  onChange={(e) => setFideRating(e.target.value)}
+                  fullWidth
                 />
               </td>
-              <td>
-                <TitleDropdown
-                  id="titleInput"
-                  name="title"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                />
-              </td>
-              <HelpBox
-                placement="bottom"
-                name={helpboxNames.createAccountsInputs}
-                text={Translated.byKey("createAccountsInputsHelpbox")}
-                show={true}
-              >
-                <td>
-                  <input
-                    type="number"
-                    id="fideRatingInput"
-                    name="fide_rating"
-                    value={fide_rating}
-                    onChange={(e) => setFideRating(e.target.value)}
-                  />
-                </td>
-              </HelpBox>
-
-              <td>
-                <FederationDropdown
-                  id="fideFederationInput"
-                  name="fide_federation"
-                  value={fide_federation}
-                  onChange={(e) => setFideFederation(e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="date"
-                  id="birthDateInput"
-                  name="birth_date"
-                  value={birth_date}
-                  onChange={(e) => setBirthDate(e.target.value)}
-                  required
-                />
-              </td>
-              <td>
-                <SexDropdown
-                  id="sexInput"
-                  name="sex"
-                  value={sex}
-                  onChange={(e) => setSex(e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </td>
-              <td>
-                <select
-                  id="level"
-                  name="level"
-                  value={level}
-                  onChange={(e) => setLevel(e.target.value)}
-                >
-                  <option value="0">Player</option>
-                  {(user.info?.level || 0) > 1 && (
-                    <>
-                      <option value="1">Club Manager</option>
-                      {(user.info?.level || 0) > 2 && (
-                        <option value="2">Org Manager</option>
-                      )}
-                    </>
+            </HelpBox>
+          </Grid>
+          <Grid item xs={12} lg={3} md={4}>
+            <MuiFederationDropdown
+              id="fideFederationInput"
+              name="fide_federation"
+              value={fide_federation}
+              onChange={(e) => setFideFederation(e.target.value)} />
+          </Grid>
+          <Grid item xs={12} lg={9} md={8}>
+            <MuiTitleDropdown
+              id="titleInput"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)} />
+          </Grid>
+          <Grid item xs={12} lg={3} md={4}>
+            <NativeSelect
+              id="level"
+              name="level"
+              value={level}
+              onChange={(e) => setLevel(e.target.value)}
+            >
+              <option value="0">Player</option>
+              {(user.info?.level || 0) > 1 && (
+                <>
+                  <option value="1">Club Manager</option>
+                  {(user.info?.level || 0) > 2 && (
+                    <option value="2">Org Manager</option>
                   )}
-                </select>
-              </td>
-              <td>
-                <WithUserOrgsClubs>
-                  <SelectClubs
-                    value={selectedClub}
-                    onChange={(value: string) => setSelectedClub(value)}
-                  />
-                </WithUserOrgsClubs>
-              </td>
-              <td>
-                <HelpBox
-                  placement="bottom"
-                  name={helpboxNames.createAccountsAction}
-                  text={Translated.byKey("createAccountsActionHelpbox")}
-                  show={true}
-                >
-                  <button
-                    type="submit"
-                    className="btn btn-primary"
-                    id="addButton"
-                  >
-                    +
-                  </button>
-                </HelpBox>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                </>
+              )}
+            </NativeSelect>
+          </Grid>
+          <Grid item xs={12} lg={9} md={8}>
+            <MuiSexDropdown
+              id="sexInput"
+              name="sex"
+              value={sex}
+              onChange={(e) => setSex(e.target.value)} />
+          </Grid>
+          <Grid item xs={12} lg={3} md={4}>
+            <WithUserOrgsClubs>
+              <SelectClubs
+                value={selectedClub}
+                onChange={(value: string) => setSelectedClub(value)}
+              />
+            </WithUserOrgsClubs>
+          </Grid>
+          <Grid item xs={12} lg={9} md={8}>
+            <input
+              type="date"
+              id="birthDateInput"
+              name="birth_date"
+              value={birth_date}
+              onChange={(e) => setBirthDate(e.target.value)}
+              required
+            />
+          </Grid>
+        </Grid>
+        <br />
+        <HelpBox
+          placement="bottom"
+          name={helpboxNames.createAccountsAction}
+          text={Translated.byKey("createAccountsActionHelpbox")}
+          show={true}
+        >
+          <Button
+            id="addButton"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Create Account
+          </Button>
+        </HelpBox>
+        <br />
+        <br />
+        {accounts.length ?
+          <DataGrid
+            autoHeight
+            rows={accountData}
+            columns={accountsColumns}
+          /> : ""
+        }
       </form>
-
       <h3 className="mt-5">
         <Translated str="passwords" />
       </h3>
